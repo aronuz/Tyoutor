2bee41fa2f9813535c49b238e1c3405c8de879b3
Anatoliy <aronuz1@hotmail.com>
2021-10-19 15:09:38 -0400

    REST authentication setup

--- Tyoutor/urls.py
+++ Tyoutor/urls.py
@@ -24,5 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/register/', RegistrationView.as_view(form_class=CustomUserForm, success_url='/'),
          name='django_registration_register'),
-    path('accounts/', include('django.contrib.auth.urls'))  # Django login views
+    path('accounts/', include('django.contrib.auth.urls')),  # Django login views
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
