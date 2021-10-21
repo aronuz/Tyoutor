a5b875f73777483cc55cd4b548fc68aa26b60e47
Anatoliy <aronuz1@hotmail.com>
2021-10-20 20:32:58 -0400

    Pages and header component initial setup

---
+++ ui/src/pages/tutors/TutorDetail.vue
@@ -0,0 +1,5 @@
+<template>
+  Tutor details
+  <router-view></router-view>
+  <router-link to="/tutors/c1/contact">Contact</router-link>
+</template>
---
+++ ui/src/pages/tutors/TutorsList.vue
@@ -0,0 +1,12 @@
+<template>
+  <section>filter</section>
+  <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Tutor registration</router-link>
+    </div>
+    <ul>
+      List of tutors
+    </ul>
+  </section>
+</template>
