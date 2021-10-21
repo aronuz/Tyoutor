534a7fc771f619dd1f8a1e1556b2004184af3858
Anatoliy <aronuz1@hotmail.com>
2021-10-21 13:59:07 -0400

    Tutors list page and detail component

--- ui/src/pages/tutors/TutorsList.vue
+++ ui/src/pages/tutors/TutorsList.vue
@@ -5,8 +5,48 @@
       <button>Refresh</button>
       <router-link to="/register">Tutor registration</router-link>
     </div>
-    <ul>
-      List of tutors
+    <ul v-if="hasTutors">
+      <tutor-item
+        v-for="tutor in filteredTutors"
+        :key="tutor.id"
+        :id="tutor.id"
+        :first-name="tutor.firstName"
+        :last-name="tutor.lastName"
+        :rate="tutor.hourlyRate"
+        :areas="tutor.areas"
+      ></tutor-item>
     </ul>
+    <h3 v-else>No tutors found.</h3>
   </section>
 </template>
+
+<script>
+import TutorItem from "../../components/tutors/TutorItem.vue";
+
+export default {
+  components: {
+    TutorItem,
+  },
+  computed: {
+    filteredTutors() {
+      return this.$store.getters["tutors/tutors"];
+    },
+    hasTutors() {
+      return this.$store.getters["tutors/hasTutors"];
+    },
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
