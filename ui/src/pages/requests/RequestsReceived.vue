78ac11f9bc66fe06adc8ebf6bbc03bc81f5e91f9
Anatoliy <aronuz1@hotmail.com>
2021-10-25 11:53:39 -0400

    Message requests vuex setup

--- ui/src/pages/requests/RequestsReceived.vue
+++ ui/src/pages/requests/RequestsReceived.vue
@@ -1,1 +1,54 @@
-<template>Requests</template>
+<template>
+  <section>
+    <ui-card>
+      <header>
+        <h2>Incoming messages</h2>
+      </header>
+      <ul v-if="hasRequests">
+        <request-item
+          v-for="req in receivedRequests"
+          :key="req.id"
+          :email="req.userEmail"
+          :message="req.message"
+        >
+        </request-item>
+      </ul>
+      <h3 v-else>There are no new messages.</h3>
+    </ui-card>
+  </section>
+</template>
+
+<script>
+import RequestItem from "@/components/requests/RequestItem.vue";
+
+export default {
+  components: {
+    RequestItem,
+  },
+  computed: {
+    receivedRequests() {
+      return this.$store.getters["requests/requests"];
+    },
+    hasRequests() {
+      return this.$store.getters["requests/hasRequests"];
+    },
+  },
+};
+</script>
+
+<style scoped>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
