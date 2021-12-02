8d8e8fe57feca6f83eefde0f26f3130322f35c1d
Anatoliy <aronuz1@hotmail.com>
2021-12-02 14:08:45 -0500

    Badge formatting

--- tyoutor-ui/src/components/ui_blocks/Badge.vue
+++ tyoutor-ui/src/components/ui_blocks/Badge.vue
@@ -1,5 +1,5 @@
 <template>
-  <span class="badge" :class="getClass(type)">
+  <span class="badge" :class="[getClass]">
     {{ text }}
   </span>
   <ui-button v-if="isOwnArea" @click="removeArea()"> Remove </ui-button>
@@ -11,26 +11,30 @@ export default {
   data() {
     return {
       classes: {
-        "a-f": /^a/,
-        "g-m": /^g/,
-        "n-t": /^n/,
-        "u-z": /^u/,
+        "a-f": /^[a-fA-F]/,
+        "g-m": /^[g-mG-M]/,
+        "n-t": /^[n-tN-T]/,
+        "u-z": /^[u-zU-Z]/,
       },
     };
   },
   computed: {
+    getClass() {
+      const obj = this.classes;
+      const area = this.type;
+      let badgeClass = "other";
+      for (let key in obj) {
+        if (obj[key].test(area)) {
+          badgeClass = key;
+        }
+      }
+      return badgeClass;
+    },
     text() {
       return this.type.toUpperCase();
     },
   },
   methods: {
-    getClass(area) {
-      const obj = this.classes;
-      const r = Object.fromEntries(
-        Object.entries(obj).filter(([key]) => obj[key].test(area))
-      );
-      return Object.keys(r)[0];
-    },
     removeArea() {
       this.emitter.emit("remove-area", { area: this.type });
     },
@@ -40,12 +44,16 @@ export default {
 
 <style scoped>
 .badge {
-  background-color: #ccc;
   color: #252525;
   border-radius: 30px;
   padding: 0.5rem 1.5rem;
   display: inline-block;
   margin-right: 0.5rem;
+}
+
+.other {
+  background-color: #ccc;
+  color: white;
 }
 
 .a-f {
@@ -55,16 +63,16 @@ export default {
 
 .g-m {
   background-color: #f1b900;
-  color: white;
+  color: #9d0000;
 }
 
 .n-t {
   background-color: #788d00;
-  color: white;
+  color: #140909;
 }
 
 .u-z {
   background-color: #04af98;
-  color: white;
+  color: #140909;
 }
 </style>
